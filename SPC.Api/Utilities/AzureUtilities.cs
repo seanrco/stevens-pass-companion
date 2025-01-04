25776a80c77eb9d69e479fc227bc54f345518249
Sean C. <sean.coffelt@gmail.com>
2025-01-04 00:26:57 -0800

    https://github.com/seanrco/stevens-pass-companion/issues/108

---
+++ SPC.Api/Utilities/AzureUtilities.cs
@@ -0,0 +1,20 @@
+﻿namespace SPC.Api.Utilities;
+
+public static class AzureUtilities
+{
+
+    /// <summary>
+    /// Gets Azure function app environment variable value.
+    /// </summary>
+    /// <remarks>
+    /// Info: https://learn.microsoft.com/en-us/azure/azure-functions/functions-dotnet-class-library?tabs=v4%2Ccmd#environment-variables
+    /// </remarks>
+    /// <param name="name">string</param>
+    /// <returns>string</returns>
+    public static string GetEnvironmentVariable(string name)
+    {
+        return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process) ?? string.Empty;
+    }
+
+
+}
